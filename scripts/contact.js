280f38434df298d8a9578645995d3392e991b5ab
JuantMartinez17 <juantomasm0@gmail.com>
2025-02-08 17:00:28 -0300

    feat: update HTML form and JS function to send email via server

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,8 +2,38 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contact-form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        console.log('Formulario enviado!');
-        alert('Gracias por tu mensaje. Me pondré en contacto contigo pronto.');
-        form.reset();
+        
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const subject = document.getElementById('subject').value;
+        const message = document.getElementById('message').value;
+
+        const formData = {
+            name: name,
+            email: email,
+            subject: subject,
+            body: message
+        }
+
+        fetch('http://localhost:3000/send-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success){
+                alert('Thanks for your interest. I will contact you soon.');
+                form.reset();
+            } else{
+                alert('There was an error sending the email. Please try again later.');
+            }
+        })
+        .catch(err => {
+            console.error('Error sending email', err);
+            alert('There was an error sending the email. Please try again later.');
+        })
     });
 });
