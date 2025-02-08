466cf40b46f375ecc5f510303193f4a766349850
JuantMartinez17 <juantomasm0@gmail.com>
2025-02-08 16:13:11 -0300

    linked form submit to contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('formulario-contacto');
+    const form = document.getElementById('contact-form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        console.log('Formulario enviado!');
         alert('Gracias por tu mensaje. Me pondré en contacto contigo pronto.');
         form.reset();
     });
